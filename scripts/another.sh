79dc19ababa8698a8b4c2caf652715f1e51c275b
Nikos Atlas <nikatlas@gmail.com>
2022-05-13 12:51:35 +0300

    ASD

--- scripts/another.sh
+++ scripts/another.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-echo "another script here"
+if [ "$1" = 'stop' ]
+  then
+    echo "Closing all ssh tunnels running on this machine"
+    TEMP_FILE="/tmp/temp-ps-aux"
+    ps aux > $TEMP_FILE
+    grep -i "sl aws tunnel" < $TEMP_FILE | awk '{ print $2 }' | xargs kill
+    exit 0
+fi
 
-# wget sl-cli
-# install deps
-# run sl aws tunnel APP
+pip install sl-cli==1.0+sl.1 -i https://sl:435cjHTdzvpg@nexus.sector.sh/repository/pypi/pypi > /dev/null
+
+if [ "$1" = 'start' ]
+  then
+    APP=$2
+    if [ -z "$APP" ]
+      then
+        echo "No app provided. Usage: ./tunnel.sh start APP_NAME"
+        exit 1
+    fi
+
+    sl aws tunnel $APP > /dev/null &
+fi
